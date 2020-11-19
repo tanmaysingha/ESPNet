accc0dad0eada2f7ea06ad3e83c9f9425beabf50
Tanmay Singha <66204567+tanmaysingha@users.noreply.github.com>
2020-11-19 21:26:28 +0800

    Add files via upload

---
+++ models/base_ESPNet.py
@@ -0,0 +1,580 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from functools import reduce
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+import tensorflow.keras.layers as KL
+from tensorflow.keras.initializers import Initializer
+from tensorflow.python.keras.utils.generic_utils import get_custom_objects
+
+import os
+import re
+import collections
+import math
+import six
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+import tensorflow.keras.models as KM
+
+import tensorflow as tf
+from tensorflow import keras
+
+#### Custom function for conv2d: conv_block
+def conv_block(inputs, conv_type, kernel, kernel_size, strides, padding='same', relu=True, reg=keras.regularizers.l2(0.00004)):
+  
+  if(conv_type == 'ds'):
+    x = tf.keras.layers.SeparableConv2D(kernel, kernel_size, padding=padding, strides = strides)(inputs)
+  else:
+    x = tf.keras.layers.Conv2D(kernel, kernel_size, padding=padding, strides = strides, kernel_regularizer=reg, bias_regularizer=reg)(inputs)  
+  
+  x = tf.keras.layers.BatchNormalization()(x)
+  
+  if (relu):
+    x = tf.keras.activations.relu(x)
+  
+  return x
+
+''' ## Credits
+All credits are due to https://github.com/qubvel/efficientnet
+Thanks so much for your contribution!
+
+## Usage:
+Adding this utility script to your kernel, and you will be able to 
+use all models just like standard Keras pretrained model. For details see
+https://www.kaggle.com/c/aptos2019-blindness-detection/discussion/100186
+
+## Pretrained Weights
+https://www.kaggle.com/ratthachat/efficientnet-keras-weights-b0b5/
+'''
+
+
+from tensorflow.keras.utils import get_file
+
+MEAN_RGB = [0.485 * 255, 0.456 * 255, 0.406 * 255]
+STDDEV_RGB = [0.229 * 255, 0.224 * 255, 0.225 * 255]
+
+MAP_INTERPOLATION_TO_ORDER = {
+    "nearest": 0,
+    "bilinear": 1,
+    "biquadratic": 2,
+    "bicubic": 3,
+}
+
+
+class EfficientConv2DKernelInitializer(Initializer):
+    """Initialization for convolutional kernels.
+    The main difference with tf.variance_scaling_initializer is that
+    tf.variance_scaling_initializer uses a truncated normal with an uncorrected
+    standard deviation, whereas here we use a normal distribution. Similarly,
+    tf.contrib.layers.variance_scaling_initializer uses a truncated normal with
+    a corrected standard deviation.
+    Args:
+      shape: shape of variable
+      dtype: dtype of variable
+      partition_info: unused
+    Returns:
+      an initialization for the variable
+    """
+
+    def __call__(self, shape, dtype=K.floatx(), **kwargs):
+        kernel_height, kernel_width, _, out_filters = shape
+        fan_out = int(kernel_height * kernel_width * out_filters)
+        return tf.random.normal(
+            shape, mean=0.0, stddev=np.sqrt(2.0 / fan_out), dtype=dtype
+        )
+
+
+class EfficientDenseKernelInitializer(Initializer):
+    """Initialization for dense kernels.
+    This initialization is equal to
+      tf.variance_scaling_initializer(scale=1.0/3.0, mode='fan_out',
+                                      distribution='uniform').
+    It is written out explicitly here for clarity.
+    Args:
+      shape: shape of variable
+      dtype: dtype of variable
+    Returns:
+      an initialization for the variable
+    """
+
+    def __call__(self, shape, dtype=K.floatx(), **kwargs):
+        """Initialization for dense kernels.
+        This initialization is equal to
+          tf.variance_scaling_initializer(scale=1.0/3.0, mode='fan_out',
+                                          distribution='uniform').
+        It is written out explicitly here for clarity.
+        Args:
+          shape: shape of variable
+          dtype: dtype of variable
+        Returns:
+          an initialization for the variable
+        """
+        init_range = 1.0 / np.sqrt(shape[1])
+        return tf.random.uniform(shape, -init_range, init_range, dtype=dtype)
+
+
+conv_kernel_initializer = EfficientConv2DKernelInitializer()
+dense_kernel_initializer = EfficientDenseKernelInitializer()
+
+
+get_custom_objects().update(
+    {
+        "EfficientDenseKernelInitializer": EfficientDenseKernelInitializer,
+        "EfficientConv2DKernelInitializer": EfficientConv2DKernelInitializer,
+    }
+)
+
+class Swish(KL.Layer):
+    def call(self, inputs):
+        return tf.nn.swish(inputs)
+
+
+class DropConnect(KL.Layer):
+    def __init__(self, drop_connect_rate=0.0, **kwargs):
+        super().__init__(**kwargs)
+        self.drop_connect_rate = drop_connect_rate
+
+    def call(self, inputs, training=None):
+        def drop_connect():
+            keep_prob = 1.0 - self.drop_connect_rate
+
+            # Compute drop_connect tensor
+            batch_size = tf.shape(inputs)[0]
+            random_tensor = keep_prob
+            random_tensor += tf.random.uniform(
+                [batch_size, 1, 1, 1], dtype=inputs.dtype
+            )
+            binary_tensor = tf.floor(random_tensor)
+            output = tf.math.divide(inputs, keep_prob) * binary_tensor
+            return output
+
+        return K.in_train_phase(drop_connect, inputs, training=training)
+
+    def get_config(self):
+        config = super().get_config()
+        config["drop_connect_rate"] = self.drop_connect_rate
+        return config
+
+
+get_custom_objects().update({"DropConnect": DropConnect, "Swish": Swish})
+
+
+GlobalParams = collections.namedtuple(
+    "GlobalParams",
+    [
+        "batch_norm_momentum",
+        "batch_norm_epsilon",
+        "dropout_rate",
+        "data_format",
+        "num_classes",
+        "width_coefficient",
+        "depth_coefficient",
+        "depth_divisor",
+        "min_depth",
+        "drop_connect_rate",
+    ],
+)
+GlobalParams.__new__.__defaults__ = (None,) * len(GlobalParams._fields)
+
+BlockArgs = collections.namedtuple(
+    "BlockArgs",
+    [
+        "kernel_size",
+        "num_repeat",
+        "input_filters",
+        "output_filters",
+        "expand_ratio",
+        "id_skip",
+        "strides",
+        "se_ratio",
+    ],
+)
+# defaults will be a public argument for namedtuple in Python 3.7
+# https://docs.python.org/3/library/collections.html#collections.namedtuple
+BlockArgs.__new__.__defaults__ = (None,) * len(BlockArgs._fields)
+
+
+class BlockDecoder(object):
+    """Block Decoder for readability."""
+
+    def _decode_block_string(self, block_string):
+        """Gets a block through a string notation of arguments."""
+        assert isinstance(block_string, str)
+        ops = block_string.split("_")
+        options = {}
+        for op in ops:
+            splits = re.split(r"(\d.*)", op)
+            if len(splits) >= 2:
+                key, value = splits[:2]
+                options[key] = value
+
+        if "s" not in options or len(options["s"]) != 2:
+            raise ValueError("Strides options should be a pair of integers.")
+
+        return BlockArgs(
+            kernel_size=int(options["k"]),
+            num_repeat=int(options["r"]),
+            input_filters=int(options["i"]),
+            output_filters=int(options["o"]),
+            expand_ratio=int(options["e"]),
+            id_skip=("noskip" not in block_string),
+            se_ratio=float(options["se"]) if "se" in options else None,
+            strides=[int(options["s"][0]), int(options["s"][1])],
+        )
+
+    def _encode_block_string(self, block):
+        """Encodes a block to a string."""
+        args = [
+            "r%d" % block.num_repeat,
+            "k%d" % block.kernel_size,
+            "s%d%d" % (block.strides[0], block.strides[1]),
+            "e%s" % block.expand_ratio,
+            "i%d" % block.input_filters,
+            "o%d" % block.output_filters,
+        ]
+        if block.se_ratio > 0 and block.se_ratio <= 1:
+            args.append("se%s" % block.se_ratio)
+        if block.id_skip is False:
+            args.append("noskip")
+        return "_".join(args)
+
+    def decode(self, string_list):
+        """Decodes a list of string notations to specify blocks inside the network.
+    Args:
+      string_list: a list of strings, each string is a notation of block.
+    Returns:
+      A list of namedtuples to represent blocks arguments.
+    """
+        assert isinstance(string_list, list)
+        blocks_args = []
+        for block_string in string_list:
+            blocks_args.append(self._decode_block_string(block_string))
+        return blocks_args
+
+    def encode(self, blocks_args):
+        """Encodes a list of Blocks to a list of strings.
+    Args:
+      blocks_args: A list of namedtuples to represent blocks arguments.
+    Returns:
+      a list of strings, each string is a notation of block.
+    """
+        block_strings = []
+        for block in blocks_args:
+            block_strings.append(self._encode_block_string(block))
+        return block_strings
+
+
+def efficientnet(
+    width_coefficient=None,
+    depth_coefficient=None,
+    dropout_rate=0.2,
+    drop_connect_rate=0.2,
+):
+    """Creates a efficientnet model."""
+    blocks_args = [
+        "r1_k3_s11_e1_i32_o16_se0.25",
+        "r2_k3_s22_e6_i16_o24_se0.25",
+        "r2_k5_s22_e6_i24_o40_se0.25",
+        "r3_k3_s22_e6_i40_o80_se0.25",
+        "r3_k5_s11_e6_i80_o112_se0.25",
+        "r4_k5_s22_e6_i112_o192_se0.25",
+        "r1_k3_s11_e6_i192_o320_se0.25",
+    ]
+    global_params = GlobalParams(
+        batch_norm_momentum=0.99,
+        batch_norm_epsilon=1e-3,
+        dropout_rate=0.2,
+        drop_connect_rate=drop_connect_rate,
+        data_format="channels_last",
+        num_classes=1000,
+        width_coefficient=1,
+        depth_coefficient=1,
+        depth_divisor=8,
+        min_depth=None,
+    )
+    decoder = BlockDecoder()
+    return decoder.decode(blocks_args), global_params
+
+
+def round_filters(filters, global_params):
+    """Round number of filters based on depth multiplier."""
+    orig_f = filters
+    multiplier = global_params.width_coefficient
+    divisor = global_params.depth_divisor
+    min_depth = global_params.min_depth
+    if not multiplier:
+        return filters
+
+    filters *= multiplier
+    min_depth = min_depth or divisor
+    new_filters = max(min_depth, int(filters + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_filters < 0.9 * filters:
+        new_filters += divisor
+    # print('round_filter input={} output={}'.format(orig_f, new_filters))
+    return int(new_filters)
+
+
+def round_repeats(repeats, global_params):
+    """Round number of filters based on depth multiplier."""
+    multiplier = global_params.depth_coefficient
+    if not multiplier:
+        return repeats
+    return int(math.ceil(multiplier * repeats))
+
+
+def SEBlock(block_args, global_params):
+    num_reduced_filters = max(1, int(block_args.input_filters * block_args.se_ratio))
+    filters = block_args.input_filters * block_args.expand_ratio
+    if global_params.data_format == "channels_first":
+        channel_axis = 1
+        spatial_dims = [2, 3]
+    else:
+        channel_axis = -1
+        spatial_dims = [1, 2]
+
+    def block(inputs):
+        x = inputs
+        x = KL.Lambda(lambda a: K.mean(a, axis=spatial_dims, keepdims=True))(x)
+        x = KL.Conv2D(
+            num_reduced_filters,
+            kernel_size=[1, 1],
+            strides=[1, 1],
+            kernel_initializer=conv_kernel_initializer,
+            padding="same",
+            use_bias=True,
+        )(x)
+        x = Swish()(x)
+        # Excite
+        x = KL.Conv2D(
+            filters,
+            kernel_size=[1, 1],
+            strides=[1, 1],
+            kernel_initializer=conv_kernel_initializer,
+            padding="same",
+            use_bias=True,
+        )(x)
+        x = KL.Activation("sigmoid")(x)
+        out = KL.Multiply()([x, inputs])
+        return out
+
+    return block
+
+
+def MBConvBlock(block_args, global_params, drop_connect_rate=None):
+    batch_norm_momentum = global_params.batch_norm_momentum
+    batch_norm_epsilon = global_params.batch_norm_epsilon
+
+    if global_params.data_format == "channels_first":
+        channel_axis = 1
+        spatial_dims = [2, 3]
+    else:
+        channel_axis = -1
+        spatial_dims = [1, 2]
+
+    has_se = (
+        (block_args.se_ratio is not None)
+        and (block_args.se_ratio > 0)
+        and (block_args.se_ratio <= 1)
+    )
+
+    filters = block_args.input_filters * block_args.expand_ratio
+    kernel_size = block_args.kernel_size
+
+    def block(inputs):
+
+        if block_args.expand_ratio != 1:
+            x = KL.Conv2D(
+                filters,
+                kernel_size=[1, 1],
+                strides=[1, 1],
+                kernel_initializer=conv_kernel_initializer,
+                padding="same",
+                use_bias=False,
+            )(inputs)
+            x = KL.BatchNormalization(
+                axis=channel_axis,
+                momentum=batch_norm_momentum,
+                epsilon=batch_norm_epsilon,
+            )(x)
+            x = Swish()(x)
+        else:
+            x = inputs
+
+        x = KL.DepthwiseConv2D(
+            [kernel_size, kernel_size],
+            strides=block_args.strides,
+            depthwise_initializer=conv_kernel_initializer,
+            padding="same",
+            use_bias=False,
+        )(x)
+        x = KL.BatchNormalization(
+            axis=channel_axis, momentum=batch_norm_momentum, epsilon=batch_norm_epsilon
+        )(x)
+        x = Swish()(x)
+
+        if has_se:
+            x = SEBlock(block_args, global_params)(x)
+
+        # output phase
+
+        x = KL.Conv2D(
+            block_args.output_filters,
+            kernel_size=[1, 1],
+            strides=[1, 1],
+            kernel_initializer=conv_kernel_initializer,
+            padding="same",
+            use_bias=False,
+        )(x)
+        x = KL.BatchNormalization(
+            axis=channel_axis, momentum=batch_norm_momentum, epsilon=batch_norm_epsilon
+        )(x)
+
+        if block_args.id_skip:
+            if (
+                all(s == 1 for s in block_args.strides)
+                and block_args.input_filters == block_args.output_filters
+            ):
+                # only apply drop_connect if skip presents.
+                if drop_connect_rate:
+                    x = DropConnect(drop_connect_rate)(x)
+                x = KL.Add()([x, inputs])
+        return x
+
+    return block
+
+def pyramid_pooling_block(input_tensor, bin_sizes):
+  concat_list = [input_tensor]
+  w = 16
+  h = 16
+
+  for bin_size in bin_sizes:
+    x = tf.keras.layers.AveragePooling2D(pool_size=(w//bin_size, h//bin_size), strides=(w//bin_size, h//bin_size))(input_tensor)
+    x = tf.keras.layers.Conv2D(80, 3, 2, padding='same')(x)
+    x = tf.keras.layers.Lambda(lambda x: tf.image.resize(x, (w,h)))(x)
+
+    concat_list.append(x)
+
+  return tf.keras.layers.concatenate(concat_list)
+
+
+def EfficientNet(
+    input_shape, block_args_list, global_params, input_tensor=None, include_top=True, pooling=None
+):
+    batch_norm_momentum = global_params.batch_norm_momentum
+    batch_norm_epsilon = global_params.batch_norm_epsilon
+    if global_params.data_format == "channels_first":
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    # Stem part
+    """if input_tensor is None:
+        inputs = KL.Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            inputs = KL.Input(tensor=input_tensor, shape=input_shape)
+        else:
+            inputs = input_tensor"""
+    x = input_tensor
+    #ff_layer1 = conv_block(x, 'conv', 128, (1,1), padding='same', strides= (1,1), relu=False)
+    x = KL.Conv2D(
+        filters=round_filters(32, global_params),
+        kernel_size=[3, 3],
+        strides=[2, 2],
+        kernel_initializer=conv_kernel_initializer,
+        padding="same",
+        use_bias=False,
+    )(x)
+    x = KL.BatchNormalization(
+        axis=channel_axis, momentum=batch_norm_momentum, epsilon=batch_norm_epsilon
+    )(x)
+    x = Swish()(x)
+
+    # Blocks part
+    block_idx = 1
+    n_blocks = sum([block_args.num_repeat for block_args in block_args_list])
+    drop_rate = global_params.drop_connect_rate or 0
+    drop_rate_dx = drop_rate / n_blocks
+
+    for block_args in block_args_list:
+        assert block_args.num_repeat > 0
+        # Update block input and output filters based on depth multiplier.
+        block_args = block_args._replace(
+            input_filters=round_filters(block_args.input_filters, global_params),
+            output_filters=round_filters(block_args.output_filters, global_params),
+            num_repeat=round_repeats(block_args.num_repeat, global_params),
+        )
+
+        # The first block needs to take care of stride and filter size increase.
+        x = MBConvBlock(
+            block_args, global_params, drop_connect_rate=drop_rate_dx * block_idx
+        )(x)
+
+        if block_args.num_repeat > 1:
+            block_args = block_args._replace(
+                input_filters=block_args.output_filters, strides=[1, 1]
+            )
+
+        for _ in xrange(block_args.num_repeat - 1):
+            x = MBConvBlock(
+                block_args, global_params, drop_connect_rate=drop_rate_dx * block_idx
+            )(x)
+               
+    # Head part
+     
+    x = pyramid_pooling_block(x, [1,2,3,6])
+    
+
+    #upsample
+    
+    final_feature = tf.keras.layers.UpSampling2D((8,8))(x)
+   
+    #Step4: Classifier
+
+    classifier = tf.keras.layers.SeparableConv2D(128, (3, 3), padding='same', strides = (1, 1), name = 'DSConv1_classifier')(final_feature)
+    classifier = tf.keras.layers.BatchNormalization()(classifier)
+    classifier = tf.keras.activations.relu(classifier)
+
+    classifier = tf.keras.layers.SeparableConv2D(128, (3, 3), padding='same', strides = (1, 1), name = 'DSConv2_classifier')(classifier)
+    classifier = tf.keras.layers.BatchNormalization()(classifier)
+    classifier = tf.keras.activations.relu(classifier)
+
+
+    classifier = conv_block(classifier, 'conv', 20, (1, 1), strides=(1, 1), padding='same', relu=False)
+
+    classifier = tf.keras.layers.Dropout(0.3)(classifier)
+
+    classifier = tf.keras.layers.UpSampling2D((4, 4))(classifier)
+    classifier = tf.keras.activations.softmax(classifier)
+
+    
+    outputs = classifier
+
+    return outputs
+
+
+
+def model(num_classes=19, input_size=(512, 512, 3)):
+
+  # Input Layer
+  input_layer = tf.keras.layers.Input(shape=input_size, name = 'input_layer')
+
+  block_agrs_list, global_params = efficientnet(width_coefficient=1.0, depth_coefficient=1.0, dropout_rate=0.2, drop_connect_rate=0.2)
+  default_input_shape = 512
+  enet = EfficientNet(
+      default_input_shape,
+      block_agrs_list,
+      global_params,
+      input_tensor=input_layer,
+      include_top=False,
+      pooling=None,)  
+
+  fast_scnn = tf.keras.Model(inputs = input_layer , outputs = enet, name = 'Fast_SCNN')
+
+  return fast_scnn
